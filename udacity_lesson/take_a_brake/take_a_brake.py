0eb02b84c8e6dc6be2e1692669b509d8890a5eef
wuchenxu <wu_chenxu@126.com>
2017-01-22 19:11:51 +0800

    use define, not magic numbers

--- udacity_lesson/take_a_brake/take_a_brake.py
+++ udacity_lesson/take_a_brake/take_a_brake.py
@@ -3,12 +3,14 @@ import webbrowser
 
 total_breaks = 3
 break_count = 0
+break_time_in_sec = 10
+url = "http://www.youtube.com/watch?v=dQw4w9WgXcQ"
 
 print("This program starts at "+time.ctime())
 while (break_count < total_breaks):
-	time.sleep(10)
+	time.sleep(break_time_in_sec)
 	print("[%d]time to break, now is[%s]." %(break_count, time.ctime()))
-	webbrowser.open("http://www.youtube.com/watch?v=dQw4w9WgXcQ")
+	webbrowser.open(url)
 	break_count = break_count + 1
 
 print("This programm ends at"+time.ctime())
