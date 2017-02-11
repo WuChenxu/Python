52e5782975365e6cf505c7218595bc733009be9e
wuchenxu <wu_chenxu@126.com>
2017-02-11 20:35:51 +0800

    add movie show website mini-project

---
+++ udacity_lesson/movie_website/entertainment_center.py
@@ -0,0 +1,35 @@
+import media
+import fresh_tomatoes
+
+toy_story = media.Movie("Toy Story", 
+                        "A story of a boy and his toys that come to life",
+                        "https://upload.wikimedia.org/wikipedia/zh/d/dc/Movie_poster_toy_story.jpg",
+                        "https://www.youtube.com/watch?v=vwyZH85NQC4")
+#print(toy_story.storyline)
+
+avatar = media.Movie("Avatar",
+                     "A marine on a alien planet",
+                     "https://upload.wikimedia.org/wikipedia/zh/b/b0/Avatar-Teaser-Poster.jpg",
+                     "https://www.youtube.com/watch?v=aVdO-cx-McA")
+
+#print(avatar.storyline)
+#avatar.show_trailer()
+avatar = media.Movie("Avatar",
+                     "A marine on a alien planet",
+                     "https://upload.wikimedia.org/wikipedia/zh/b/b0/Avatar-Teaser-Poster.jpg",
+                     "https://www.youtube.com/watch?v=aVdO-cx-McA")
+dahuaxiyou = media.Movie("A Chinese Odyssey",
+						 "A monkey king's love story",
+						 "https://upload.wikimedia.org/wikipedia/zh/0/0c/A_Chinese_Odyssey_2.jpg",
+						 "https://www.youtube.com/watch?v=LnNKNApIOAo")
+#print(dahuaxiyou.storyline)
+
+#dahuaxiyou.show_trailer()
+lengeds_of_the_fall = media.Movie("Legends of the Fall",
+								  "Three brothers and their father living in the wilderness and plains of Montana in the early 20th century and how their lives are affected by nature, history, war and love.",
+								  "https://upload.wikimedia.org/wikipedia/en/1/1d/Legendsoffallposter.jpg",
+								  "https://www.youtube.com/watch?v=ocAeBZdSDWg")
+movies = [toy_story, avatar, dahuaxiyou, lengeds_of_the_fall]
+
+fresh_tomatoes.open_movies_page(movies)
+
