5c028a9d62e99567685c4cc68afa74071b494487
wuchenxu <wu_chenxu@126.com>
2017-02-14 21:25:46 +0800

    re-factory Movie class which inherit from Video class

--- udacity_lesson/movie_website/entertainment_center.py
+++ udacity_lesson/movie_website/entertainment_center.py
@@ -2,23 +2,23 @@ import media
 import fresh_tomatoes
 
 toy_story = media.Movie("Toy Story", 
+                        123,
                         "A story of a boy and his toys that come to life",
                         "https://upload.wikimedia.org/wikipedia/zh/d/dc/Movie_poster_toy_story.jpg",
                         "https://www.youtube.com/watch?v=vwyZH85NQC4")
 #print(toy_story.storyline)
 
 avatar = media.Movie("Avatar",
+                        200,
                      "A marine on a alien planet",
                      "https://upload.wikimedia.org/wikipedia/zh/b/b0/Avatar-Teaser-Poster.jpg",
                      "https://www.youtube.com/watch?v=aVdO-cx-McA")
 
 #print(avatar.storyline)
 #avatar.show_trailer()
-avatar = media.Movie("Avatar",
-                     "A marine on a alien planet",
-                     "https://upload.wikimedia.org/wikipedia/zh/b/b0/Avatar-Teaser-Poster.jpg",
-                     "https://www.youtube.com/watch?v=aVdO-cx-McA")
+
 dahuaxiyou = media.Movie("A Chinese Odyssey",
+                            122,
 						 "A monkey king's love story",
 						 "https://upload.wikimedia.org/wikipedia/zh/0/0c/A_Chinese_Odyssey_2.jpg",
 						 "https://www.youtube.com/watch?v=LnNKNApIOAo")
@@ -26,10 +26,16 @@ dahuaxiyou = media.Movie("A Chinese Odyssey",
 
 #dahuaxiyou.show_trailer()
 lengeds_of_the_fall = media.Movie("Legends of the Fall",
+                                    222,
 								  "Three brothers and their father living in the wilderness and plains of Montana in the early 20th century and how their lives are affected by nature, history, war and love.",
 								  "https://upload.wikimedia.org/wikipedia/en/1/1d/Legendsoffallposter.jpg",
 								  "https://www.youtube.com/watch?v=ocAeBZdSDWg")
 movies = [toy_story, avatar, dahuaxiyou, lengeds_of_the_fall]
 
 fresh_tomatoes.open_movies_page(movies)
-
+print(media.Movie.VALID_RATINGS)
+print(media.Movie.__doc__)
+print(media.Movie.__module__)
+print(media.Movie.__name__)
+print(media.Movie.__dict__)
+print(media.Movie.__bases__)
--- udacity_lesson/movie_website/media.py
+++ udacity_lesson/movie_website/media.py
@@ -1,9 +1,33 @@
 import webbrowser
 
-class Movie():
-    def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube):
-        self.title = movie_title
-        self.storyline = movie_storyline
+class Video():
+    '''This calls provides a way to store video related infomation'''
+    VALID_RATINGS = ['G', 'PG', 'PG-13', 'R']
+    def __init__(self, title, duration):
+        self.title = title
+        self.duration = duration
+
+
+class Movie(Video):
+    '''This class provides a way to store movie related infomation'''
+
+    
+    # http://www2.lib.uchicago.edu/keith/courses/python/class/5/
+    # https://docs.python.org/2/reference/datamodel.html
+    '''
+    Attribute   Type    Read/Write  Description
+    __dict__    dictionary  R/W The class name space.
+    __name__    string  R/O The name of the class.
+    __bases__   tuple of classes    R/O The classes from which this class inherits.
+    __doc__ string OR None  R/W The class documentation string.
+    __module__  string  R/W The name of the module in which this class was defined.
+
+    '''
+    # modify module name
+    __module__ = 'Hello'
+    
+    def __init__(self, movie_title, duration, movie_storyline, poster_image, trailer_youtube):
+        Video.__init__(self, movie_title, duration)
         self.poster_image_url = poster_image
         self.trailer_youtube_url = trailer_youtube
     def show_trailer(self):
